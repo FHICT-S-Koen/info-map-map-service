d6a5f4839cdcfac09e4ef64cbf92f45fd4b3c28f
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-10-06 15:24:32 +0200

    Secured the API with Oauth2 and added the possibility to create a new map

---
+++ src/main/java/com/infomap/mapservice/map/MapController.java
@@ -0,0 +1,22 @@
+package com.infomap.mapservice.map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/")
+@CrossOrigin
+public class MapController {
+
+    private final MapService mapService;
+
+    @Autowired
+    public MapController(MapService mapService) {
+        this.mapService = mapService;
+    }
+
+    @PostMapping("private")
+    public void createMap(@RequestBody Map map) {
+        mapService.createMap(map);
+    }
+}
