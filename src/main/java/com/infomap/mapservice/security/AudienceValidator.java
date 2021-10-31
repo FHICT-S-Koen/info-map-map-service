e9fce01abb27191f476b1437e364b0a710bca6b1
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-10-31 18:33:25 +0100

    Update AudienceValidator.java

--- src/main/java/com/infomap/mapservice/security/AudienceValidator.java
+++ src/main/java/com/infomap/mapservice/security/AudienceValidator.java
@@ -1,5 +1,6 @@
 package com.infomap.mapservice.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
@@ -9,6 +10,7 @@ import org.springframework.security.oauth2.jwt.Jwt;
 class AudienceValidator implements OAuth2TokenValidator<Jwt> {
     private final String audience;
 
+    @Autowired
     AudienceValidator(String audience) {
         this.audience = audience;
     }
