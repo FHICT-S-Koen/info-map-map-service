692a277a04e175998464cb4e40dc6816039d7357
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-10-31 18:32:59 +0100

    Update MapController.java

--- src/main/java/com/infomap/mapservice/map/MapController.java
+++ src/main/java/com/infomap/mapservice/map/MapController.java
@@ -1,5 +1,6 @@
 package com.infomap.mapservice.map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,6 +9,7 @@ public class MapController {
 
     private final MapService mapService;
 
+    @Autowired
     public MapController(MapService mapService) {
         this.mapService = mapService;
     }
